e8d55c613be0e97e0d0ae78618f795979298686e
Georgy <goshan@Georgys-MacBook-Pro.local>
2023-12-21 18:01:09 +0300

    Revert "Initial commit"
    This reverts commit 8d397507e9d281f82343d2035fbe4df04b9e50e2.

--- hexlet_django_blog/article/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from hexlet_django_blog.article import views
-
-urlpatterns = [
-    path("", views.IndexPageView.as_view(template_name="articles.html"), name="articles_index"),
-]
